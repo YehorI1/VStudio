4ecc0c7a12b5b0ecf329bea3d526b52e9e2747d0
YehorI1 <ikonnikovyehor@gmail.com>
2026-06-16 11:00:10 +0200

    commit_CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1",'http://dashboard.integrator.space']

--- tut_project/settings.py
+++ tut_project/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-r%$+fbk!p_92ero0l8xlb_8@nns=3$-c+iy=bd6vd*=64ivjz@
 DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1",'dashboard.integrator.space']
-CSRF_TRUSTED_ORIGINS = ["127.0.0.1",'dashboard.integrator.space']
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1",'http://dashboard.integrator.space']
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "home"
